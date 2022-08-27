d82371aeaaeff92da9b218cb692d98b1e8d8ef2c
v1cthor <victor-04-29@outlook.com>
2022-08-27 17:31:56 -0500

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MachineComponent } from './component/machine/machine.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Module PrimeNg
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {MessagesModule} from 'primeng/messages';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    DialogModule,
+    InputTextModule,
+    ButtonModule,
+    MessagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/machine/machine.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { Message, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-machine',
+  templateUrl: './machine.component.html',
+  styleUrls: ['./machine.component.css'],
+  providers: [MessageService]
+})
+export class MachineComponent implements OnInit {
+  showName: boolean = true;
+  showProductoSelected: boolean = false;
+  showtipos: boolean = false;
+  errorName: boolean = false;
+  showIcon: boolean = false;
+  showCambio: boolean = false;
+  showIngresado: boolean = false;
+  name: string = '';
+
+  costo: number = 0;
+  tipo: string = '';
+
+  Moneda_1: number = 10;
+  Moneda_2: number = 50;
+  Moneda_3: number = 100;
+
+  coinI: number = 0;
+
+  cambioIcon: number = 0;
+
+  msgs1!: Message[];
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  validaName() {
+    if (this.name.length > 1) {
+      this.showName = false;
+      this.showtipos = true;
+      this.errorName = false;
+    } else { this.errorName = true; }
+
+  }
+
+  productoSelected(producto: string) {
+    if (producto === 'A') { this.tipo = 'A'; this.costo = 270 }
+    else if (producto === 'B') { this.tipo = 'B'; this.costo = 340 }
+    else if (producto === 'C') { this.tipo = 'C'; this.costo = 390 }
+    this.showtipos = false;
+    this.showProductoSelected = true;
+    this.showIcon = true;
+  }
+
+  entercoin(coin: number) {
+    this.showIngresado = true;
+    this.messageService.add({ key: 'tl',severity: 'info', summary: '', detail: '$'+coin });
+
+    this.coinI += coin;
+    if (this.coinI >= this.costo) {
+      this.showIcon = false;
+      this.cambio();
+    }
+
+    if (this.coinI === this.costo) {
+      this.showCambio = true;
+      this.messageService.add({key: 'tls',  severity: 'success', summary: '', detail: '$0, Ingresaste la cantidad exacta' });
+    }
+  }
+
+  cambio() {
+    if (this.coinI > this.costo) {
+      this.cambioIcon = this.coinI - this.costo;
+      while (this.cambioIcon > 0) {
+
+        if (this, this.cambioIcon >= 50) {
+          this.showCambio = true;
+          this.cambioCoin(50);
+          this.cambioIcon = this.cambioIcon - 50;
+
+        }
+        else if (this.cambioIcon < 50) {
+          this.showCambio = true;
+          this.cambioCoin(10);
+          this.cambioIcon = this.cambioIcon - 10;
+        }
+      }
+    }
+  }
+
+  cambioCoin(numero: number) {
+    this.messageService.add({key: 'tls', severity: 'success', summary: '', detail: '$' + numero });
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
+}
